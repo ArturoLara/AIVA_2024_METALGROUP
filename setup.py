3eae262183d5d79ef648ba2693c6e339a7e477d8
Arturo Lara <a.lara.2024@alumnos.urjc.es>
2025-02-25 16:28:20 +0100

    initial structure - test, Impl, requeriments, setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+ name='Metal',
+ version='1.0',
+ description='Detección de desperfectos en planchas metalicas por visión artificial',
+ author='Adrián Cobo y Arturo Lara',
+ packages=['metal'],
+ install_requires=['numpy', 'pandas']
+)
