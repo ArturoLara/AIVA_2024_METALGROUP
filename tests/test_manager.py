1fda62e526cfcc8177af63850f30996fbd486f57
Arturo Lara <a.lara.2024@alumnos.urjc.es>
2025-04-09 17:24:08 +0200

    removed exampleMethods in tests

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -15,8 +15,8 @@ class TestMainManager(unittest.TestCase):
 
         # Mock de la configuración
         self.mock_config = {
-            "preprocessing_methods": ["ExamplePreprocessingMethod", "CannyMethod"],
-            "detector_methods": "ExampleDetectionMethod"
+            "preprocessing_methods": ["UmbralizeMethod", "CannyMethod"],
+            "detector_methods": "ContrastMethod"
         }
 
         # Mock de una imagen de prueba (200x200)
@@ -59,7 +59,7 @@ class TestMainManager(unittest.TestCase):
     def test_main_manager_invalid_detector_method(self, mock_read_image, mock_parse_config):
         # Configuración con un método de detección inválido
         invalid_config = {
-            "preprocessing_methods": ["ExamplePreprocessingMethod"],
+            "preprocessing_methods": ["CannyMethod"],
             "detector_methods": "InvalidDetectionMethod"
         }
 
